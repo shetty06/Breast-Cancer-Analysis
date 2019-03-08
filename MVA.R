81c7df53ab28d0074c1aad02ac71cea3ab2b1cb4
shetty06 <47369711+shetty06@users.noreply.github.com>
2019-03-08 16:28:21 -0500

    Add files via upload

--- MVA.R
+++ MVA.R
@@ -154,3 +154,153 @@ leveneTest(concavity_mean~ diagnosis, data=breast_cancer)
 leveneTest(points_mean ~ diagnosis, data=breast_cancer)
 leveneTest(symmetry_mean ~ diagnosis, data=breast_cancer)
 leveneTest(dimension_mean ~ diagnosis, data=breast_cancer)
+
+#PCA
+
+dim(breast_cancer)
+attach(breast_cancer)
+head(breast_cancer)
+#Get the Correlations between the measurements
+cor(breast_cancer[-2])
+c <- (cor(breast_cancer[-2]))
+plot(c)
+# Using prcomp to compute the principal components (eigenvalues and eigenvectors). With scale=TRUE, variable means are set to zero, and variances set to one
+breast_cancer_pca <- prcomp(breast_cancer[,-2],scale=TRUE)
+breast_cancer_pca
+plot(breast_cancer_pca)
+summary(breast_cancer_pca)
+#View(breast_cancer_pca)
+head(breast_cancer_pca$x)
+# sample scores stored in breast_cancer_pca$x
+# singular values (square roots of eigenvalues) stored in breast_cancer_pca$sdev
+# loadings (eigenvectors) are stored in breast_cancer_pca$rotation
+# variable means stored in breast_cancer_pca$center
+# variable standard deviations stored in sparrows_pca$scale
+# A table containing eigenvalues and %'s accounted, follows
+# Eigenvalues are sdev^2
+(eigen_breast_cancer <- breast_cancer_pca$sdev^2) ## brackets for print
+names(eigen_breast_cancer) <- paste("PC",1:31,sep="")
+eigen_breast_cancer
+sumlambdas <- sum(eigen_breast_cancer)
+sumlambdas
+propvar <- eigen_breast_cancer/sumlambdas
+propvar
+summary(eigen_breast_cancer)
+summary(breast_cancer_pca)
+cumvar_breast_cancer <- cumsum(propvar)
+cumvar_breast_cancer
+matlambdas <- rbind(eigen_breast_cancer,propvar,cumvar_breast_cancer)
+rownames(matlambdas) <- c("Eigenvalues","Prop. variance","Cum. prop. variance")
+round(matlambdas,4)
+summary(breast_cancer_pca)
+breast_cancer_pca$rotation
+print(breast_cancer_pca)
+# Sample scores stored in breast_cancer_pca$x
+head(breast_cancer_pca$x)
+# Identifying the scores by their diagnosis
+diag_pca <- cbind(data.frame(diagnosis),breast_cancer_pca$x)
+head(diag_pca)
+# Means of scores for all the PC's classified by diagnosis status
+tabmeansPC <- aggregate(diag_pca[,2:31],by=list(diagnosis=breast_cancer$diagnosis),mean)
+tabmeansPC
+tabmeansPC <- tabmeansPC[rev(order(tabmeansPC$diagnosis)),]
+tabmeansPC
+tabfmeans <- t(tabmeansPC[,-1])
+tabfmeans
+colnames(tabfmeans) <- t(as.vector(tabmeansPC[1]))
+tabfmeans
+# Standard deviations of scores for all the PC's classified by diagnosis status
+tabsdsPC <- aggregate(diag_pca[,2:31],by=list(breast_cancer$diagnosis),sd)
+tabfsds <- t(tabsdsPC[,-1])
+colnames(tabfsds) <- t(as.vector(tabsdsPC[1]))
+tabfsds
+t.test(PC1~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC2~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC3~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC4~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC5~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC6~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC7~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC8~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC9~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC10~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC11~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC12~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC13~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC14~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC15~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC16~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC17~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC18~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC19~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC20~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC21~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC22~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC23~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC24~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC25~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC26~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC27~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC28~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC29~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC30~breast_cancer$diagnosis,data=diag_pca)
+t.test(PC31~breast_cancer$diagnosis,data=diag_pca)
+
+# F ratio tests
+var.test(PC1~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC2~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC3~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC4~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC5~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC6~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC7~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC8~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC9~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC10~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC11~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC12~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC13~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC14~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC15~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC16~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC17~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC18~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC19~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC20~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC21~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC22~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC23~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC24~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC25~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC26~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC27~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC28~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC29~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC30~breast_cancer$diagnosis,data=diag_pca)
+var.test(PC31~breast_cancer$diagnosis,data=diag_pca)
+# Levene's tests (one-sided)
+library(car)
+(LTPC1 <- leveneTest(PC1~breast_cancer$diagnosis,data=diag_pca))
+(p_PC1_1sided <- LTPC1[[3]][1]/2)
+(LTPC2 <- leveneTest(PC2~breast_cancer$diagnosis,data=diag_pca))
+(p_PC2_1sided=LTPC2[[3]][1]/2)
+(LTPC3 <- leveneTest(PC3~breast_cancer$diagnosis,data=diag_pca))
+(p_PC3_1sided <- LTPC3[[3]][1]/2)
+(LTPC4 <- leveneTest(PC4~breast_cancer$diagnosis,data=diag_pca))
+(p_PC4_1sided <- LTPC4[[3]][1]/2)
+(LTPC5 <- leveneTest(PC5~breast_cancer$diagnosis,data=diag_pca))
+(p_PC5_1sided <- LTPC5[[3]][1]/2)
+# Plotting the scores for the first and second components
+plot(diag_pca$PC1, diag_pca$PC2,pch=ifelse(diag_pca$diagnosis == "S",1,16),xlab="PC1", ylab="PC2", main="569 entries against values for PC1 & PC2")
+abline(h=0)
+abline(v=0)
+legend("bottomleft", legend=c("Benign","Malignant"), pch=c(1,16))
+plot(eigen_breast_cancer, xlab = "Component number", ylab = "Component variance", type = "l", main = "Scree diagram")
+plot(log(eigen_breast_cancer), xlab = "Component number",ylab = "log(Component variance)", type="l",main = "Log(eigenvalue) diagram")
+print(summary(breast_cancer_pca))
+#View(breast_cancer_pca)
+diag(cov(breast_cancer_pca$x))
+xlim <- range(breast_cancer_pca$x[,1])
+head(breast_cancer_pca$x[,1])
+head(breast_cancer_pca$x)
+plot(breast_cancer_pca$x,xlim=xlim,ylim=xlim)
